814e5c673f30a3a8831fc79bd574205b6e8df772
red-eyed-99 <mgnblack99@gmail.com>
2024-09-24 08:31:01 +0300

    feat: add currency and exchange rate models

---
+++ src/main/java/models/Currency.java
@@ -0,0 +1,40 @@
+package models;
+
+public class Currency {
+    private int id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+}
---
+++ src/main/java/models/ExchangeRate.java
@@ -0,0 +1,40 @@
+package models;
+
+public class ExchangeRate {
+    private int id;
+    private int baseCurrencyId;
+    private int targetCurrencyId;
+    private double rate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getBaseCurrencyId() {
+        return baseCurrencyId;
+    }
+
+    public void setBaseCurrencyId(int baseCurrencyId) {
+        this.baseCurrencyId = baseCurrencyId;
+    }
+
+    public int getTargetCurrencyId() {
+        return targetCurrencyId;
+    }
+
+    public void setTargetCurrencyId(int targetCurrencyId) {
+        this.targetCurrencyId = targetCurrencyId;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+}
