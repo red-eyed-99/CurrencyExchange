0474b3d805578827e81f3f037bccfe82d74735e6
red-eyed-99 <mgnblack99@gmail.com>
2024-09-26 15:18:47 +0300

    feat: implement getting currency by code, save new currency in DB, checking the availability of currency in the database

--- src/main/java/dao/CurrencyDAO.java
+++ src/main/java/dao/CurrencyDAO.java
@@ -1,27 +1,54 @@
 package dao;
 
-import utils.SQLiteConnector;
-import exceptions.DataReadException;
+import exceptions.NotFoundException;
+import datasource.SQLiteConnector;
+import exceptions.QueryExecuteException;
 import exceptions.DatabaseConnectionException;
 import models.Currency;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class CurrencyDAO implements DAO<Currency> {
 
     @Override
-    public Optional<Currency> get(Integer id) {
-        return Optional.empty();
+    public Currency get(String code) throws
+            DatabaseConnectionException, QueryExecuteException, NotFoundException {
+
+        String sql = "SELECT * FROM Currencies WHERE code = ?";
+
+        Currency currency = null;
+
+        try (Connection connection = SQLiteConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, code);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    currency = new Currency();
+                    currency.setId(resultSet.getInt("id"));
+                    currency.setCode(resultSet.getString("code"));
+                    currency.setName(resultSet.getString("full_name"));
+                    currency.setSign(resultSet.getString("sign"));
+                }
+            }
+
+            if (currency == null) {
+                throw new NotFoundException("Currency with code " + code + " not found");
+            }
+
+        } catch (SQLException e) {
+            throw new QueryExecuteException();
+        }
+
+        return currency;
     }
 
     @Override
-    public List<Currency> getAll() throws DatabaseConnectionException, DataReadException {
+    public List<Currency> getAll() throws
+            DatabaseConnectionException, QueryExecuteException, NotFoundException {
+
         String sql = "SELECT * FROM Currencies";
 
         List<Currency> currencies = new ArrayList<>();
@@ -29,18 +56,24 @@ public class CurrencyDAO implements DAO<Currency> {
         try (Connection connection = SQLiteConnector.getConnection();
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery(sql)) {
+
             while (resultSet.next()) {
                 Currency currency = new Currency();
 
                 currency.setId(resultSet.getInt("id"));
                 currency.setCode(resultSet.getString("code"));
-                currency.setFullName(resultSet.getString("full_name"));
+                currency.setName(resultSet.getString("full_name"));
                 currency.setSign(resultSet.getString("sign"));
 
                 currencies.add(currency);
             }
+
+            if (currencies.isEmpty()) {
+                throw new NotFoundException("Not found list of currencies");
+            }
+
         } catch (SQLException e) {
-            throw new DataReadException();
+            throw new QueryExecuteException();
         }
 
         return currencies;
@@ -52,7 +85,43 @@ public class CurrencyDAO implements DAO<Currency> {
     }
 
     @Override
-    public void save(Currency item) {
+    public void save(Currency currency) throws DatabaseConnectionException, QueryExecuteException {
+        String sql = "INSERT INTO Currencies (code, full_name, sign) VALUES (?, ?, ?)";
 
+        try (Connection connection = SQLiteConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            preparedStatement.setString(1, currency.getCode());
+            preparedStatement.setString(2, currency.getName());
+            preparedStatement.setString(3, currency.getSign());
+
+            preparedStatement.executeUpdate();
+
+            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    currency.setId(resultSet.getInt(1));
+                }
+            }
+
+        } catch (SQLException e) {
+            throw new QueryExecuteException();
+        }
+    }
+
+    public boolean checkExistence(String code) throws DatabaseConnectionException, QueryExecuteException {
+        String sql = "SELECT * FROM Currencies WHERE code = ?";
+
+        try (Connection connection = SQLiteConnector.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            preparedStatement.setString(1, code);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultSet.next();
+            }
+
+        } catch (SQLException e) {
+            throw new QueryExecuteException();
+        }
     }
 }
